3b91e02d3c11540fdda0c6833fd242facb42f727
Matiserrano99 <matiasserrano99@gmail.com>
2023-04-15 15:10:42 -0300

    adding files

--- pages/index.js
+++ pages/index.js
@@ -1,124 +1,141 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { ethers, providers } from "ethers";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  // walletConnected keep track of whether the user's wallet is connected or not
+  const [walletConnected, setWalletConnected] = useState(false);
+  // Create a reference to the Web3 Modal (used for connecting to metamask) which
+  // persists as long as the page is open
+  const web3ModalRef = useRef();
+  // ENS
+  const [ens, setENS] = useState("");
+  // Save the address of the currently connected account
+  const [address, setAddress] = useState("");
+
+  /**
+   * Sets the ENS, if the current connected address has an associated ENS or else it sets
+   * the address of the connected account.
+   */
+  const setENSOrAddress = async (address, web3Provider) => {
+    // lookup the ENS related to the given address
+    let _ens = await web3Provider.lookupAddress(address);
+    // If the address has an ENS set the ENS or else just set the address
+    if(_ens) {
+      setENS(_ens);
+    } else {
+      setAddress(address);
+    }
+  };
+
+  /**
+   * A `Provider` is needed to interact with the blockchain - reading transaction,
+   * reading balances, reading state, etc.
+   * 
+   * A `Signer` is a special type of Provider used in case a `write` transaction
+   * needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent.
+   * Metamask exposes a Singer API to allow your website to request signatures from the
+   * user using Signer functions.
+   */
+  const getProviderOrSigner = async () => {
+    // Connect to metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value
+    // to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the goerli network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if(chainId !== 5){
+      window.alert("Change the network to goerli");
+      throw new Error("Change network to goerli");
+    }
+    const signer = web3Provider.getSigner();
+    // Get the address associated to the signer which is connected to Metamask
+    const address = await signer.getAddress();
+    // Calls the function to set the ENS or Address
+    await setENSOrAddress(address, web3Provider);
+    return signer;
+  };
+
+  /*
+    connectWallet: Connects the Metamask wallet
+  */
+ const connectWallet = async () =>{
+  try {
+    // Get the provider from web3Modal, which in our case is metamask
+    // When used for the first time, it prompts the user to connect their wallet
+    await getProviderOrSigner(true);
+    setWalletConnected(true);
+  } catch(err) {
+    console.error(err);
+  }
+ };
+
+ /*
+  renderButton: Returns a button based on the state of the drop
+ */
+
+  const renderButton = ()=> {
+    if (walletConnected) {
+      <div>Wallet connected</div>;
+    } else {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect your wallet
+        </button>
+      );
+    }
+  };
+  // useEffects are used to react to changes in state of the website
+  // The array at the end of function call represents what state changes will
+  // trigger this effect
+  // In this case, whenever the value of `walletConnected` changes - this effect
+  // will be called
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the
+    // Metamask wallet
+    if(!walletConnected) {
+      // Assing the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <Head>
+        <title>ENS Dapp</title>
+        <meta name="description" content="ENS-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>
+            Welcome to LearnWeb3 Punks {ens ? ens : address}!
+          </h1>
+          <div className={styles.description}>
+            {/* Using HTML Entities for the apostrophe */}
+            It&#39;s an NFT collection for LearnWeb3 Punks.
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./learnweb3punks.png" />
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer className={styles.footer}>
+        Made with &#10084; by LearnWeb3 Punks
+      </footer>
+    </div>
+  );
 }
